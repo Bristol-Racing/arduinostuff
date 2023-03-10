a6d4718ddfd8b7d7f893cd7d060f6208b3409892
TBExtent <jedpreist@gmail.com>
2023-03-10 15:23:56 +0000

    logging

--- plot.py
+++ plot.py
@@ -15,54 +15,68 @@ axs = []
 
 colours = ["red", "green", "blue"]
 
+logFile = "logs/log.csv"
+
 window = 30
 startTime = time.time()
 
-for i, sensor in enumerate(sensors):
-    yss.append([])
-    ax = fig.add_subplot(1, len(sensors), i + 1)
-    ax.set_title(sensor)
-    axs.append(ax)
+with open(logFile, "w") as log:
+    log.write("time")
 
-def animate(i):
-    data = None
-    try:
-        data = [float(i) for i in serial.readline().decode().strip().split(",")]
-    except:
-        pass
+    for i, sensor in enumerate(sensors):
+        log.write(f",{sensor}")
+        yss.append([])
+        ax = fig.add_subplot(1, len(sensors), i + 1)
+        ax.set_title(sensor)
+        axs.append(ax)
 
-    if data is not None:
-        newTime = time.time() - startTime
-        xs.append(newTime)
+    log.write("\n")
+    log.flush()
 
-        windowStart = 0
-        while xs[windowStart] < newTime - window:
-            windowStart += 1
+    def animate(i):
+        data = None
+        dataLine = None
+        try:
+            dataLine = serial.readline().decode().strip()
+            data = [float(i) for i in dataLine.split(",")]
+        except:
+            pass
 
-        # print(windowStart)
+        if data is not None:
+            newTime = time.time() - startTime                   
+            xs.append(newTime)
 
-        newXs = xs[windowStart:]
-        print(len(xs))
-        print(len(newXs))
-        print()
+            log.write(f"{newTime},{dataLine}\n")
+            log.flush()
 
-        xs.clear()
-        xs.extend(newXs)
-        
-        for i, val in enumerate(data):
-            yss[i].append(val)
-            newYs = yss[i][windowStart:]
-            yss[i].clear()
-            yss[i].extend(newYs)
+            windowStart = 0
+            while xs[windowStart] < newTime - window:
+                windowStart += 1
 
-        for i, ys in enumerate(yss):
-            axs[i].clear()
-            axs[i].set_title(sensors[i])
-            axs[i].plot(xs, ys, color=colours[i])
-            axs[i].set_xlim(newTime - window, newTime)
+            # print(windowStart)
+
+            newXs = xs[windowStart:]
+            print(len(xs))
+            print(len(newXs))
+            print()
+
+            xs.clear()
+            xs.extend(newXs)
+            
+            for i, val in enumerate(data):
+                yss[i].append(val)
+                newYs = yss[i][windowStart:]
+                yss[i].clear()
+                yss[i].extend(newYs)
+
+            for i, ys in enumerate(yss):
+                axs[i].clear()
+                axs[i].set_title(sensors[i])
+                axs[i].plot(xs, ys, color=colours[i])
+                axs[i].set_xlim(newTime - window, newTime)
 
 
-serial = Serial("/dev/ttyUSB0", 57600)
+    serial = Serial("/dev/ttyUSB0", 57600)
 
-ani = animation.FuncAnimation(fig, animate, interval = 100)
-plt.show()
+    ani = animation.FuncAnimation(fig, animate, interval = 100)
+    plt.show()
