1d8bef13794f99fbcbeee92270f889c52aa570ee
Richard Hughes <112817796+richardhughes01@users.noreply.github.com>
2023-06-02 15:15:42 +0100

    Update ADC_Current.hpp

--- dyno/sensor/ADC_Current.hpp
+++ dyno/sensor/ADC_Current.hpp
@@ -3,11 +3,9 @@
 #ifndef CURRENT_H
 #define CURRENT_H
 
-// #define VIN A0
-const float vcc    = 5.00;// supply voltage 5V or 3.3V
-const float factor = 0.02;// 20mV/A is the factor
-// int ledPin = 3;      // LED connected to digital pin 9
-// int val = 0;         // variable to store the read value
+#include <Wire.h>
+#include <Adafruit_ADS1X15.h>
+
 
 #include "sensor.hpp"
 
@@ -18,6 +16,7 @@ namespace Sensor {
 
         long totalReading;
         int readings;
+        Adafruit_ADS1115 ads1115;
     public:
         CurrentSensor(uint8_t pin);
         ~CurrentSensor();
@@ -26,10 +25,16 @@ namespace Sensor {
         double read();
     };
 
-    CurrentSensor::CurrentSensor(uint8_t pin) {
-        vin = pin;
-        pinMode(pin, INPUT);
+    CurrentSensor::CurrentSensor(uint8_t) {
+        ads1115.begin();
 
+        //  ads1115.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV (default)
+
+        // ads1115.setGain(GAIN_ONE);     // 1x gain   +/- 4.096V  1 bit = 2mV
+        // ads1115.setGain(GAIN_TWO);     // 2x gain   +/- 2.048V  1 bit = 1mV
+         ads1115.setGain(GAIN_FOUR);    // 4x gain   +/- 1.024V  1 bit = 0.5mV
+        // ads1115.setGain(GAIN_EIGHT);   // 8x gain   +/- 0.512V  1 bit = 0.25mV
+        // ads1115.setGain(GAIN_SIXTEEN); // 16x gain  +/- 0.256V  1 bit = 0.125mV
         totalReading = 0;
         readings = 0;
     }
@@ -39,14 +44,17 @@ namespace Sensor {
     }
 
     void CurrentSensor::tick() {
-        totalReading += analogRead(vin);
+        int16_t results;
+
+  results = ads1115.readADC_Differential_0_1();
+        totalReading += results;
         readings++;
     }
 
     double CurrentSensor::read() {
         //Robojax.com ACS758 Current Sensor 
         double average = (double)totalReading / (double)readings;
-        double voltage = (5.0 / 1023.0) * (double)average;// Read the voltage from sensor
+        // double voltage = (5.0 / 1023.0) * (double)average;// Read the voltage from sensor
         // double a = voltage - (vcc * 0.5) + 0.007;// 0.007 is a value to make voltage zero when there is no current
         // double current = voltage / a;
 
@@ -54,7 +62,7 @@ namespace Sensor {
         readings = 0;
 
         // return current;
-        return voltage;
+        return average;
     }
 }
 
